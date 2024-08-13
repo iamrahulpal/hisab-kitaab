0fa798bcec121e711f4da8778d5e5ef8aaf73219
Rahul <87530955+iamrahulpal@users.noreply.github.com>
2024-08-13 22:32:15 +0530

    Revert "Revert 3 feature/onboarding"

---
+++ src/app/onboard/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Illustration from "../../../public/Images/Illustration.png";
+import Signup from "@/components/onboarding/Signup";
+import Login from "@/components/onboarding/Login";
+
+export default function Onboard() {
+  return (
+    <main className=" ">
+      <div className=" flex flex-col items-center justify-between h-screen">
+        <div className=" flex flex-col items-center justify-center">
+          <div className="pt-8">
+            <Image
+              src={Illustration}
+              alt="Illustration"
+              width={312}
+              height={312}
+              priority={true}
+            />
+          </div>
+          <div className="px-[49px] space-y-[17px]">
+            <h1 className=" text-3xl font-bold text-center text-[#212325]">
+              Gain total control of your money
+            </h1>
+            <p className=" text-base text-[#91919F] text-center">
+              Become your own money manager and make every cent count
+            </p>
+          </div>
+        </div>
+        <div className=" space-y-4 mb-2">
+          <Signup />
+          <Login />
+        </div>
+      </div>
+    </main>
+  );
+}
