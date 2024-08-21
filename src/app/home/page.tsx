195643bf28f386ef5e439d084bb973146c25f394
iamrahulpal <rahulgurupal@gmail.com>
2024-08-21 16:19:38 +0530

    NAVBAR ADDED

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/home/Navbar";
+
+export default function Home() {
+  return (
+    <>
+    <div className="bg-gradient-to-b from-[#FFF6E5]">
+      <Navbar />
+    </div>
+    </>
+  );
+}
---
+++ src/components/home/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { RxChevronDown } from "react-icons/rx";
+import { FaBell } from "react-icons/fa";
+export default function Navbar() {
+  return (
+    <div className="py-3 px-4 flex justify-between items-center bg-gradient-to-b from-[#FFF6E5]">
+      {/* Avatar */}
+      <button className=" ring-2 ring-[#7F3DFF] rounded-full ring-offset-2">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </button>
+      {/* Current Month */}
+      <button className=" flex items-center space-x-1 border border-[#F1F1FA] py-2 pl-2 pr-4 rounded-[40px]">
+        <RxChevronDown className=" h-6 w-6" />
+        <p className=" text-[#212325] text-sm">February</p>
+      </button>
+      {/* Notification */}
+      <FaBell className=" h-8 w-8 text-[#7F3DFF]" />
+    </div>
+  );
+}
